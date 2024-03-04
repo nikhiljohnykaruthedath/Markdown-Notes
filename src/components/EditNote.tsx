cc408af2225515b3d2c39776a52ad8ea1362d5ea
Nikhil Johny Karuthedath <nikhiljohnykaruthedath@Nikhils-MacBook-Pro.local>
2024-03-04 04:18:35 -0600

    test github actions

--- src/components/EditNote.tsx
+++ src/components/EditNote.tsx
@@ -12,7 +12,9 @@ export function EditNote({ onSubmit, onAddTag, availableTags }: EditNoteProps) {
 	const note = useNote();
 	return (
 		<>
-			<div className="navbar"></div>
+			<div className="navbar">
+				<div className="navbar-title">Edit Note</div>
+			</div>
 			<NoteForm
 				title={note.title}
 				markdown={note.markdown}
